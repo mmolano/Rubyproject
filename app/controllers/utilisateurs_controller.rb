521f482498982b4f55928de1d61dd0a2f3d59b5f
mmolano <miike.molano@gmail.com>
2018-10-30 23:03:03 +0100

    Messages erreurs | Close #6

--- app/controllers/utilisateurs_controller.rb
+++ app/controllers/utilisateurs_controller.rb
@@ -1,5 +1,5 @@
 class UtilisateursController < ApplicationController
-  
+
   def show
     @utilisateur = Utilisateur.find(params[:id])
 
@@ -9,5 +9,21 @@ class UtilisateursController < ApplicationController
   def new
     @utilisateur = Utilisateur.new
   end
+
+  def create
+    @utilisateur = Utilisateur.new(utilisateur_params)
+    if @utilisateur.save
+      #sauvegarde confrimé <- true
+    else 
+      render "new" 
+    end 
+  end
+
+  
+  private
+  def utilisateur_params
+    params.require(:utilisateur).permit(:nom, :email, :password, :password_confirmation)
+  end  
+
 
 end
