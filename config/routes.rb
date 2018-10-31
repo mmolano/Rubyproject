2c27c47ad8f5c4c5c07c76b595da5b6b24b9f0b8
mmolano <miike.molano@gmail.com>
2018-10-31 14:15:49 +0100

    création du controller connexion et déconnexion | Close #10

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   
 
+  get 'session/new'
+
   root 'statique#accueil'
 
   get '/contact', to: 'statique#contact'
@@ -11,6 +13,13 @@ Rails.application.routes.draw do
   get '/services', to: 'statique#services'
 
   get '/signup', to: 'utilisateurs#new'
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
+
 
   resources :utilisateurs
   
