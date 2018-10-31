3bee5ee42e9df08d117ab7b5332275c810d2bf2b
mmolano <miike.molano@gmail.com>
2018-10-31 15:07:40 +0100

    Modification menu | Close #14

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,5 +5,10 @@ module SessionsHelper
 
   def current_utilisateur
     @current_utilisateur ||= Utilisateur.find_by(id: session[:utilisateur_id])
+  end
+  
+  def logged_in?
+    !current_utilisateur.nil?
   end  
+  
 end
