4183c168f5267957f112292485058617ed5ccd1d
mmolano <miike.molano@gmail.com>
2018-11-02 01:10:14 +0100

    correction buggs #3 ..

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,7 +3,7 @@ class AccountActivationsController < ApplicationController
     utilisateur = Utilisateur.find_by(email: params[:email])
     if utilisateur && !utilisateur.activated? && utilisateur.authenticated?(:activation, params[:id])
       utilisateur.activate
-      log_in user
+      log_in utilisateur
       flash[:success] = "Compte active!"
       redirect_to utilisateur
     else
