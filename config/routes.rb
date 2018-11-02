b860c73750b3bc27fe2d916e16a7aea292830bf7
mmolano <miike.molano@gmail.com>
2018-11-02 16:44:47 +0100

    controller reset password | Close #29

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'session/new'
@@ -21,4 +25,5 @@ Rails.application.routes.draw do
   
   resources :utilisateurs 
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :update, :edit]
 end
