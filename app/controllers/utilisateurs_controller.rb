04bb6a501088d1d6c4b4ace7479a1a31d417ef37
mmolano <miike.molano@gmail.com>
2018-11-02 22:36:59 +0100

    manipulation des msgs | Close #39

--- app/controllers/utilisateurs_controller.rb
+++ app/controllers/utilisateurs_controller.rb
@@ -52,13 +52,6 @@ class UtilisateursController < ApplicationController
     params.require(:utilisateur).permit(:nom, :email, :password, :password_confirmation)
   end
 
-  def logged_in_utilisateur
-    unless logged_in?
-      flash[:danger] = 'Merci de vous connecter'
-      redirect_to login_url
-    end
-  end
-
   def correct_utilisateur
     @utilisateur = Utilisateur.find(params[:id])
     redirect_to(root_url) unless current_utilisateur?(@utilisateur)
